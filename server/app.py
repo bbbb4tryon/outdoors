1bb9e5bb49a30fe9e405fc9c3b4c4c3ee29c417d
Ben Tryon <b.t.tryon@gmail.com>
2023-05-04 13:16:30 -0600

    functional backend routing

--- server/app.py
+++ server/app.py
@@ -1,16 +1,36 @@
 #!/usr/bin/env python3
+# path: 
 
-# Standard library imports
-
-# Remote library imports
-from flask import request
+from flask import Flask, request, jsonify
 from flask_restful import Resource
+#instance of the Flask app is in config app = Flask(__name__)
 
 # Local imports
 from config import app, db, api
-from models import User, Recipe
+from models import Doer, Bringing, Recommendations
 
-# Views go here!
+@app.route('/')
+def index():
+    return '<h1>Outdoors Backend</h1>'
+
+#return a list of object from the model with API endpoints - using POSTMAN to log
+@app.route('/doers')
+def get_doers():
+    doers = Doer.query.all()
+    return jsonify([doer.to_dict() for doer in doers])
+
+
+@app.route('/what_youre_bringing')
+def get_what_youre_bringing():
+    what_youre_bringing = Bringing.query.all()
+    return jsonify([bringing.to_dict() for bringing in what_youre_bringing])
+
+
+@app.route('/our_recommendations')
+def get_our_recommendations():
+    our_recommendations = Recommendations.query.all()
+    return jsonify([recommendation.to_dict() for recommendation in our_recommendations])
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,5 +1,39 @@
+# export FLASK_APP=app.py
+# export FLASK_RUN_PORT=5555
+# flask db init
+# flask db revision --autogenerate -m 'Create tables'
+# flask db upgrade
 from sqlalchemy_serializer import SerializerMixin
-
 from config import db
 
-# Models go here!
+#NOTE: backrefs are plural
+class Doer(db.Model, SerializerMixin):
+    __tablename__='doers'
+    
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String, unique = True, nullable = False)
+    recommendations = db.relationship('Recommendations', backref = 'doers')
+    serialize_rules = ('-recommendations.doers')
+
+    def __repr__(self):
+        return f'<Doer {self.username}>'
+    
+    
+class Bringing(db.Model, SerializerMixin):
+    __tablename__='what_youre_bringing'
+    
+    id = db.Column(db.Integer, primary_key = True)
+    top = db.Column(db.String)
+    bottom = db.Column(db.String)
+    recommendations = db.relationship(
+        'Recommendations', backref='what_youre_bringing')
+    serialize_rules = ('-our_recommendations.what_youre_bringing')
+
+class Recommendations(db.Model, SerializerMixin):
+    __tablename__='our_recommendations'
+    
+    id = db.Column(db.Integer, primary_key = True)
+    doer_id = db.Column(db.Integer, db.ForeignKey('doers.id'))
+    bringing_id = db.Column(db.Integer, db.ForeignKey('what_youre_bringing.id'))
+    serialize_rules = ('-doers.our_recommendations',
+                       '-what_youre_bringing.our_recommendations')
